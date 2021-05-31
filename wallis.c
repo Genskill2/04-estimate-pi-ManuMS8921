5a3c2fb32ea8803954bdaee15bbfcb71df9d6524
ManuMS8921 <84288600+ManuMS8921@users.noreply.github.com>
2021-05-31 16:04:18 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -1,65 +1,66 @@
-import math
-import unittest
-import random
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
+float mc_pi(int);
 
-#Function wallis
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
+}
 
-def wallis(n):
-    pi = 1
-    while n:
-        pi *= (4*n**2)/(4*n**2-1)
-        n = n-1
-    return 2*pi 
+int main(void) {
+  float pi0;
+  float pi1;
+  
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+  
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
 
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
+      abort();
+  }
 
-class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-@@ -13,6 +25,22 @@ def test_high_iters(self):
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+    
+  for (int i=2000; i<5000; i++) {
+    pi0 = mc_pi(i);
+    if (!(fabs(pi0 - M_PI) < 0.4)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
+      abort();
+    }
+  }
+}
 
+float mc_pi(int n) 
 
-# monte_carlo function
+{float c=0.0;
 
-def monte_carlo(i):
-    n = i
-    j = 0
-    while n:
-        x = random.random()
-        y = random.random()
-        dist = (x**2+y**2)**0.5
-        if dist < 1:
-            j = j+1
-        n -= 1
-    pi = 4*(j/i)
-    float wallis_pi(int n) 
-{
-float ans=1.0;
+float ans; 
+
 for(int i=1;i<=n;i++) 
-{float nr= 4*i*i;
 
-ans= ans*(nr/(nr-1));
+{ 
+
+float x= frandom(); 
+
+float y= frandom(); 
+
+if(sqrt(x*x+y*y)<1) 
+
+c++; 
 
 } 
 
-return ans*2; 
-}            
-    float wallis_pi(int n) 
-{
-float ans=1.0;
-for(int i=1;i<=n;i++) 
-{float nr= 4*i*i;
+ans = 4*c/n;
 
-ans= ans*(nr/(nr-1));
+return ans; 
 
-} 
-
-return ans*2; 
-}            
-    return pi
-
-
-class TestMC(unittest.TestCase):
-    def test_randomness(self):
-        pi0 = monte_carlo(15000)
+}
